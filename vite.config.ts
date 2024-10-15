a8fddff4fdf5d994727f0dee44c7cbb7ecfcef2e
GauDan-2005 <gauravdan2005@gmail.com>
2024-10-15 08:53:03 +0530

    vite config optimiused

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+
+// Load environment variables from .env files
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,9 +11,9 @@ export default defineConfig({
   server: {
     proxy: {
       "/auth": {
-        target: process.env.VITE_BACKEND_URL, // Use process.env to access environment variables
+        target: process.env.VITE_BACKEND_URL, // Access the backend URL
         changeOrigin: true,
-        secure: true,
+        secure: false,
       },
     },
   },
